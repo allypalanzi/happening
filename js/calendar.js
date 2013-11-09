8c94a402a8074f30ecfb35c2e4013d5457e7e2a6
louisfettet <louisfettet@gmail.com>
2013-11-09 12:37:50 -0500

    moar fixes

--- js/calendar.js
+++ js/calendar.js
@@ -57,7 +57,7 @@ function makeApiCall() {
 		var eventDateTime = new Date(resp.items[i].start.dateTime);
 		var eventDate = new Date(resp.items[i].start.date);
 		var calElement;
-		if (eventDateTime == undefined) {
+		if (eventDate) {
 		    calElement = "<li><p>" +
 			eventTitle +
 			"</p><p>" +
