3b656d2c59aa28c47820b2a3d69b67e70d751dd5
ally palanzi <ampalanzi@gmail.com>
2013-11-09 01:16:26 -0500

    some more styles tabs n shit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
 
+		$('.menu-settings-toggle').on('click', function(e){
+	    $('.settings-module').fadeToggle("show hide");
+	    e.preventDefault();
+	});
+
+    //  When user clicks on tab, this code will be executed
+    $(".settings-nav li").click(function() {
+        //  First remove class "active" from currently active tab
+        $(".settings-nav li").removeClass('active');
+ 
+        //  Now add class "active" to the selected/clicked tab
+        $(this).addClass("active");
+ 
+        //  Hide all tab content
+        $(".settings-content").hide();
+ 
+        //  Here we get the href value of the selected tab
+        var selected_tab = $(this).find("a").attr("href");
+ 
+        //  Show the selected tab content
+        $(selected_tab).fadeIn();
+ 
+        //  At the end, we add return false so that the click on the link is not executed
+        return false;
+    });
+
 });
