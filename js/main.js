dd2d7f876938911d47177f26b85754fa30de03fa
ally palanzi <ampalanzi@gmail.com>
2013-11-09 06:42:15 -0500

    adding pages

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+		$('.menu-settings-toggle').on('click', function(e){
+	    $('.settings-module').fadeToggle("show hide");
+	    e.preventDefault();
+	});
+
+    $(".settings-nav li").click(function() {
+        $(".settings-nav li").removeClass('active');
+        $(this).addClass("active");
+        $(".settings-content").hide();
+        var selected_tab = $(this).find("a").attr("href");
+        $(selected_tab).fadeIn();
+        return false;
+    });
+
+    $(".colors li").click(function() {
+    	var new_color = $(this).css('background-color');
+    	$('.main').css('background-color', new_color);
+
+    });
+
+});
